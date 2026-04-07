55f8ca3fb5963dbbca34eb34c5c201bbfab2d473
Бруснигин Эдуард Андреевич <s24380091@unn.ru>
2026-04-07 11:17:55 +0300

    lab

---
+++ src/hash1.h
@@ -0,0 +1,17 @@
+// Hash table ("С открытой адресацией")
+// Brusnigin
+
+template <typename TKey, typename TValue>
+class HashTable1 {
+private:
+	void HashFunction() {
+		
+	}
+	
+public:
+	
+
+	TValue& operator[] {
+		
+	}
+}
--- src/hash2.h
+++ src/hash2.h
@@ -1,2 +1,2 @@
 // Hash table
-// ?????
+// Zaplatkin
